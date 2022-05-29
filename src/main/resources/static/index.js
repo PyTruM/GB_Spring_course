11edd41327970f6db9d8aff6a21ac59a72644adc
PyTruM <pytrum@gmail.com>
2022-05-29 11:06:36 +0400

    .

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,3 +1,5 @@
+alert("Все работает. Ура");
+
 angular.module('app', []).controller('indexController', function ($scope, $http) {
     const contextPath = 'http://localhost:8189/app';
 
@@ -7,6 +9,26 @@ angular.module('app', []).controller('indexController', function ($scope, $http)
                 $scope.ProductsList = response.data;
             });
     };
+
+    $scope.deleteProduct = function (productId) {
+        $http.get(contextPath + '/products/delete/' + productId)
+            .then(function (response) {
+                $scope.loadProducts();
+            });
+    }
+
+    $scope.changeCost = function (productId, delta) {
+        $http({
+            url: contextPath + '/products/change_cost',
+            method: 'GET',
+            params: {
+                productId: productId,
+                delta: delta
+            }
+        }).then(function (response) {
+            $scope.loadProducts();
+        });
+    }
 
     $scope.loadProducts();
 });
